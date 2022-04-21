9506b1117eeeb5842e142a9a6c74b32e524b267c
yookyung Choi <ykc131719@gmail.com>
2022-04-21 23:15:16 +0900

    220421 SOLVED

---
+++ BOJ/15649.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> vec(N , 0);
+	for (int i = 0; i < M; i++) {
+		vec[i] = 1;
+	}
+
+	do {
+		for (int i = 0; i < N; i++) {
+			if (vec[i] != 0)
+				cout << i + 1 << " ";
+		}
+		cout << endl;
+
+	} while (prev_permutation(vec.begin(), vec.end()));
+
+
+	return 0;
+}
