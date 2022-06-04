8b8facece2d3ecc307e7c25483bc859017fee0a2
yookyung Choi <ykc131719@gmail.com>
2022-06-04 19:36:46 +0900

    refactor : 1931 map 사용 불필요

--- BOJ/1931.cpp
+++ BOJ/1931.cpp
@@ -1,35 +1,30 @@
 ﻿#include <iostream>
 #include <vector>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> start_t;
-map<int, int> fin_t;
 int main() {
-	int N, num = 0;
+	int N, s, e, num = 0;
 	cin >> N;
 
-	start_t.resize(N, 0);
+	vector<pair<int, int>> t;
 	for (int i = 0; i < N; i++) {
-		int e;
-		cin >> start_t[i] >> e;
-		fin_t.insert({ i,e });
+		cin >> s >> e;
+		t.push_back(make_pair(s, e));
 	}
 
-	vector< pair<int, int>> v(fin_t.begin(), fin_t.end());
-	sort(v.begin(), v.end(), [](pair<int, int> a, pair<int, int> b) {
+	sort(t.begin(), t.end(), [](pair<int, int> a, pair<int, int> b) {
 		if (a.second == b.second) {
-			return start_t[a.first] < start_t[b.first];
+			return a.first < b.first;
 		}
 		return a.second < b.second;
 	});
 
 	int s_time = 0;
-	for (auto iter = v.begin(); iter != v.end(); iter++) {
-		if (s_time <= start_t[iter->first]) {
-			s_time = iter->second;
+	for (int i = 0; i < N; i++) {
+		if (s_time <= t[i].first) {
+			s_time = t[i].second;
 			num++;
 		}
 	}
