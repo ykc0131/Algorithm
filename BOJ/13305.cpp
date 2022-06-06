94783fc585f4d73b26bbec707023e5155cefebfa
yookyung Choi <ykc131719@gmail.com>
2022-06-06 23:29:23 +0900

    220606 58점SOVLED

---
+++ BOJ/13305.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N, c;
+	cin >> N;
+
+	vector<int> d(N - 1, 0);
+	vector<pair<int, int>> city;
+	for (int i = 0; i < N - 1; i++)
+		cin >> d[i];
+
+	for (int i = 0; i < N; i++) {
+		cin >> c;
+		city.push_back(make_pair(i, c));
+	}
+
+	int cost = city[0].second;
+	int result = 0;
+	for (int i = 0; i < N - 1; i++) {
+		int each_cost = city[i].second;
+		cost = each_cost < cost ? each_cost : cost;
+		result += cost * d[i];
+	}
+
+	cout << result;
+	return 0;
+}
