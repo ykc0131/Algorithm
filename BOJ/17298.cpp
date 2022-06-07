d01d376119cc1490978ffe2127441cc9659df4b5
yookyung Choi <ykc131719@gmail.com>
2022-06-07 17:56:21 +0900

    220607 SOLVED

---
+++ BOJ/17298.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector<int> v(N, 0), NGE(N, 0);
+	stack<int> s;
+	for (int i = 0; i < N; i++) {
+		cin >> v[i];
+	}
+
+	for (int i = N - 1; i >= 0; i--) {
+		while (!s.empty() && s.top() <= v[i])
+			s.pop();
+		if (s.empty())
+			NGE[i] = -1;
+		else 
+			NGE[i] = s.top();
+		s.push(v[i]);
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << NGE[i] << " ";
+	}
+
+	return 0;
+}
