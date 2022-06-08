313c6e37a6c04ebbcc259044e6539d71a8739b22
yookyung Choi <ykc131719@gmail.com>
2022-06-08 23:13:21 +0900

    220606 시간초과ㅠ

---
+++ BOJ/15651.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	vector<int> v_m(M, 0);
+
+	bool b = true;
+	while (b) {
+		int plus = 0;
+		for (int i = 0; i < M; i++) 
+			cout << v_m[i] + 1 << " ";
+
+		++v_m[M - 1];
+		for (int i = M - 1; i >= 0; i--) {
+			if (i>0 && ((v_m[i] + plus) == N)) {
+				v_m[i] = 0;
+				plus = 1;
+			}
+			else {
+				v_m[i] += plus;
+				plus = 0;
+			}
+
+			if (i == 0 && v_m[i] == N) 
+				b = false;
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
