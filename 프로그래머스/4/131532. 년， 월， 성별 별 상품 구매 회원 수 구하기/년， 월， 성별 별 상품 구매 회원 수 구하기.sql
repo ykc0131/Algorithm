4e47bed530a06a7a74b6c2bf27e29c9650c31706
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-02-23 16:08:29 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,42 +1,13 @@
-# -- 코드를 입력하세요
-# SELECT YEAR, MONTH, GENDER, COUNT(USER_INFO.USER_ID) AS USERS
-# FROM 
-#     USER_INFO
-#     JOIN
-#     ( 
-#         SELECT 
-#                 ONLINE_SALE_ID,
-#                 USER_ID,
-#                 date_format(SALES_DATE,'%Y') AS YEAR,
-#                 date_format(SALES_DATE,'%m') AS MONTH
-#         FROM ONLINE_SALE
-#     ) AS OS
-#     ON
-#     USER_INFO.USER_ID = OS.USER_ID
-# WHERE gender is not null
-# GROUP BY YEAR, MONTH, GENDER
-# ORDER BY YEAR ASC, MONTH ASC, GENDER ASC
-
 -- 코드를 입력하세요
-SELECT YEAR, MONTH, GENDER, COUNT(USER_ID) AS USERS
+SELECT  YEAR(SALES_DATE) AS YEAR, 
+        MONTH(SALES_DATE) AS MONTH,
+        GENDER, 
+        COUNT(DISTINCT(OS.USER_ID)) AS USERS
 FROM 
-    (
-        SELECT DISTINCT OS.USER_ID, YEAR, MONTH, GENDER
-        FROM 
-            USER_INFO
-            JOIN
-            ( 
-                SELECT 
-                        ONLINE_SALE_ID,
-                        USER_ID,
-                        date_format(SALES_DATE,'%Y') AS YEAR,
-                        date_format(SALES_DATE,'%m') AS MONTH
-                FROM ONLINE_SALE
-            ) AS OS
-            ON
-            USER_INFO.USER_ID = OS.USER_ID
-        WHERE gender is not null
-    ) AS USOS
+    ONLINE_SALE OS
+    JOIN
+    USER_INFO UI
+    USING(USER_ID)
+WHERE GENDER is not null
 GROUP BY YEAR, MONTH, GENDER
 ORDER BY YEAR ASC, MONTH ASC, GENDER ASC
-
