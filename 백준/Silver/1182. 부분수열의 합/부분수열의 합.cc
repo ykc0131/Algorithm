3a0a9ed2be73fe17a0b20788db03c7752ec2b9be
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-03-11 01:57:19 +0900

    [Silver II] Title: 부분수열의 합, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -6,43 +6,32 @@
 
 using namespace std;
 
-int N,M,cnt;
+int N,M;
 vector<int> vec;
 void init(){
     cin >> N >> M;
-    cnt = 0;
-
     vec.resize(N,0);
     for(int i=0; i<N; i++){
         cin >> vec[i];
-        if(vec[i]==M)
-            cnt++;
     }
 }
 
-vector<bool> visited;
-int result=0;
-void backtracking(int idx, int cnt, int limit, int sum){
+int result=0, limit = 0;
+void backtracking(int idx, int cnt, int sum){
     if(cnt==limit){
         if(sum==M)
             result++;
         return;
     }
 
-    for(int i=idx; i<N; i++){
-        if(!visited[i]){
-            visited[i] = true;
-            backtracking(i+1 , cnt+1, limit, sum+vec[i]);
-            visited[i] = false;
-        }
-    }
+    for(int i=idx; i<N; i++)
+        backtracking(i+1 , cnt+1, sum+vec[i]);
 }
 
 void solve(){
     for(int i=1; i<N+1; i++){ //1의 개수.
-        visited.clear();
-        visited.resize(N,false);
-        backtracking(0,0,i,0);
+        limit = i;
+        backtracking(0,0,0);
     }
     cout << result << "\n";
 }
