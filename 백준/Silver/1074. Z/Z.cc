7fa7bca1fd13c8375add2e7ac34c4c7ca5301a68
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-04-18 18:32:29 +0900

    [Silver I] Title: Z, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.cc
+++ 백준/Silver/1074. Z/Z.cc
@@ -10,42 +10,24 @@ void init() {
 	cin >> N >> r >> c;
 	maxN = pow(2, N);
 }
-
-int dx[] = { 0,1,0,1 };
-int dy[] = { 0,0,1,1 };
 int index = 0;
 int tracking(int n, int y, int x) {
-	if (y < 0 || y >= maxN || x < 0 || x >= maxN)
-		return -1;
+	if (y==r && x==c)
+		return index;
 
-	if (n == 2) {
-		for (int i = 0; i < 4; i++) {
-			int _y = y + dy[i];
-			int _x = x + dx[i];
-			
-			index++;
-			if (_y == r && _x == c) 
-				return index - 1;
-		}
+	int value = pow(n / 2, 2);
+	if (r >= y + n / 2) {
+		y += n / 2;
+		index += value * 2;
 	}
-	else {
-		int value = pow(n / 2, 2);
-		if (r < y + n / 2 && c < x + n / 2) {
-			return tracking(n / 2, y, x);
-		}
-		else if (r < y +  n / 2 && c >= x + n / 2) {
-			index += value;
-			return tracking(n / 2, y, x + n / 2);
-		}
-		else if (r >= y + n / 2 && c < x + n / 2) {
-			index += value * 2;
-			return tracking(n / 2, y + n / 2, x);
-		}
-		else if (r >= y + n / 2 && c >= x + n / 2) {
-			index += value * 3;
-			return tracking(n / 2, y + n / 2, x + n / 2);
-		}
+
+	if (c >= x + n / 2) {
+		x += n / 2 ;
+		index += value;
 	}
+
+	return tracking(n / 2, y, x);
+	
 }
 
 void solve() {
