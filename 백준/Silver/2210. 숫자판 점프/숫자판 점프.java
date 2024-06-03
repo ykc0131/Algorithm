5b0254b932f9d7b4e875b3aed87de04ee7759aeb
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-06-03 22:20:27 +0900

    [Silver II] Title: 숫자판 점프, Time: 168 ms, Memory: 19036 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int[][] nPad = new int[5][5];
+    static void init() throws IOException {
+        for(int i=0; i<5; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int j=0; j<5; j++){
+                nPad[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    static Map<String, Integer> result = new HashMap<>();
+    static int[] dy = {0,1,0,-1};
+    static int[] dx = {1,0,-1,0};
+
+    static StringBuilder sb;
+    static void backtracking(int y, int x, int cnt){
+        if(cnt==6){
+            result.put(sb.toString(),1);
+            return;
+        }
+
+        for(int i=0; i<4; i++){
+            int _y = y + dy[i];
+            int _x = x + dx[i];
+
+            if(_y < 0 || _x < 0 || _y >= 5 || _x >= 5)
+                continue;
+
+            sb.append(nPad[_y][_x]);
+            backtracking(_y, _x, cnt+1);
+            sb.deleteCharAt(cnt);
+        }
+    }
+
+    static void solve() {
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                sb = new StringBuilder();
+                backtracking(i, j, 0);
+            }
+        }
+
+        System.out.println(result.size());
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+}
