6528b44e674963a183fd8690c744e3a3ed18e693
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-09-28 16:38:05 +0900

    [Gold V] Title: 리모컨, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -1,42 +1,58 @@
 //
-// Created by yooki on 2024/03/06.
+// Created by yooki on 2024/09/28.
 //
 #include <iostream>
-#include <algorithm>
-#define INF 500000
+#include <cmath>
 
 using namespace std;
 
 int N,M;
-int nums[10] = {0,};
+int mal[10] = {0,};
+
 void init(){
     cin >> N >> M;
-
-    for(int i=0; i<M; i++) {
+    for(int i=0; i<M; i++){
         int n;
         cin >> n;
-        nums[n] = 1;
+        mal[n] = 1;
     }
-}
-
-int broken(int n){
-    string s = to_string(n);
-    for(int i=0; i<s.length(); i++){
-        if(nums[s[i]-'0']==1)
-            return true;
-    }
-    return false;
 }
 
 void solve(){
-    int minCnt = abs(N-100);
-    for(int i=0; i<=INF*2+1; i++){
-        if(!broken(i)){
-            int cnt = abs(N-i) + to_string(i).length();
-            minCnt = min(minCnt, cnt);
+    if(N==100){
+        cout << 0 << endl;
+        return;
+    }
+
+    int min = abs(N-100);
+    int s = abs(N - 100) > N ? 0 : N - abs(N - 100);
+    int e = N < 100 ? 100 : N + abs(N-100);
+
+    for(int i=s; i<e; i++) {
+        int tmp = i, cnt = 0;
+        bool cant = false;
+
+        if(i==0){
+            if(mal[tmp%10]==1)
+                cant = true;
+            cnt = 1;
+        }
+        else{
+            while(tmp!=0) {
+                if(mal[tmp%10]==1){
+                    cant = true; break;
+                }
+                tmp /= 10;
+                cnt++;
+            }
+        }
+
+        if(!cant){
+            min = min < cnt + abs(N-i) ? min : cnt + abs(N-i);
         }
     }
-    cout << minCnt << "\n";
+
+    cout << min << endl;
 }
 
 int main(){
