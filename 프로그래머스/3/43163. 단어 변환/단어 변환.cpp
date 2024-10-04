338b3b5e15493bed9bdacb43b306c63df6a04f1e
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-10-04 18:22:51 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,9 +1,45 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
-    return answer;
+int N, bIdx=-1, tIdx=-1;
+bool posi(string a, string b){
+    int cnt=0;
+    for(int i=0; i<a.length(); i++){
+        if(a[i]!=b[i]) cnt++;
+    }
+    
+    if(cnt==1)
+        return true;
+    return false;
+}
+
+int solution(string begin, string target, vector<string> words) { 
+    queue<pair<string,int>> q;
+    q.push({begin,0});
+    
+    int result = 0;
+    vector<int> vst(words.size(), 0);
+    while(!q.empty()){
+        pair<string,int> temp = q.front(); q.pop();
+        string cur = temp.first;
+        int num = temp.second;
+
+        if(cur == target){
+            result = num;
+            break;
+        }
+        
+        for(int i=0; i<words.size(); i++){
+            if(posi(cur, words[i]) &&  vst[i]==0){
+                vst[i] = 1;
+                q.push({words[i], num+1});
+            }
+        }
+    }
+    
+    return result;
 }
