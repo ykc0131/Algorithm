ddbb2e4110b7cd52a5a4acb8f888cf237e5dda43
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-10-04 17:57:11 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,80 +1,9 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <map>
-#include <queue>
 
 using namespace std;
 
-int N, bIdx=-1, tIdx=-1;
-vector<vector<int>> vec;
-map<string,int> m;
-void init(string begin, string target, vector<string> words){
-    words.push_back(begin);
-    N = words.size();
-    vec.clear();
-    vec.resize(N);
-    
-    for(int i=0; i<N; i++){
-        m[words[i]] = i;
-        for(int j=i+1; j<N; j++){
-            if(i!=j) {
-                int cnt=0;
-                for(int k=0; k<words[i].size(); k++){
-                    if(words[i][k] != words[j][k])
-                        cnt++;
-                }
-                if(cnt==1){
-                    vec[i].push_back(j);
-                    vec[j].push_back(i);
-                }
-            }
-        }
-    }
-    
-    auto bIter = m.find(begin);
-    if(bIter!=m.end())
-        bIdx = bIter->second;
-
-    auto tIter = m.find(target);
-    if(tIter!=m.end())
-        tIdx = tIter->second;
-}
-
-int result = 0;
-void bfs(){
-    queue<int> q;
-    q.push(bIdx);
-    vector<int> vst(N,-1);
-    vst[bIdx] = 0;
-    
-    while(!q.empty()){
-        int cur = q.front(); q.pop();
-
-        if(cur==tIdx)
-            break;
-        
-        for(int i=0; i<vec[cur].size(); i++){
-            int next = vec[cur][i];
-            
-            if(vst[next]==-1){
-                vst[next] = vst[cur] + 1;
-                q.push(next);
-            }
-        }
-    }
-    
-    result = vst[tIdx];
-}
-
-void solve(){
-    if(tIdx==-1)
-        return;
-    bfs();
-}
-
-int solution(string begin, string target, vector<string> words) { 
-    init(begin, target, words);
-    solve();
-    return result;
+int solution(string begin, string target, vector<string> words) {
+    int answer = 0;
+    return answer;
 }
